b3dec4b878f543be2233684fc405a42b80d807ac
chanacham <cchambre@student.touro.edu>
2023-02-17 11:26:00 -0500

    game fixes no test

--- src/main/java/chambre/morsecode/WordleGame.java
+++ src/main/java/chambre/morsecode/WordleGame.java
@@ -2,6 +2,7 @@ package chambre.morsecode;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class WordleGame {
@@ -20,11 +21,13 @@ public class WordleGame {
         wordleWord = fives.get(random.nextInt(fives.size()));
     }
 
-    public void guess(String guessString){
+    public CharResult[] guess(String guessString){
         CharResult[] status = new CharResult[5];
         for (int i = 0; i < wordleWord.length(); i++) {
             if (wordleWord.contains(guessString.substring(i, i + 1))) {
-                if (guessString.substring(i, i + 1).equalsIgnoreCase(wordleWord.substring(i, i + 1))){
+                String guess = guessString.substring(i,i+1);
+                String wordle = wordleWord.substring(i,i+1);
+                if (guess.equalsIgnoreCase(wordle)){
                     status[i] = CharResult.Correct;
                 }
                 status[i] = CharResult.WrongPlace;
@@ -32,8 +35,6 @@ public class WordleGame {
                 status[i] = CharResult.NotFound;
             }
         }
-        for (CharResult em: status) {
-            System.out.println(em);
-        }
+        return status;
     }
 }
