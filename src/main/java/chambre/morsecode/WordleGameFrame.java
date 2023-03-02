05ef629801b96350360e8d3984c942a8954db0f9
chanacham <cchambre@student.touro.edu>
2023-03-01 19:02:40 -0500

    gui more additions before mvc

--- src/main/java/chambre/morsecode/WordleGameFrame.java
+++ src/main/java/chambre/morsecode/WordleGameFrame.java
@@ -2,37 +2,39 @@ package chambre.morsecode;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Arrays;
 
 public class WordleGameFrame extends JFrame {
     JTextField[][] fields;
+    private int guessCounter;
 
     public WordleGameFrame(WordleGame currGame) {
+        //Board Layout
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout());
+        mainPanel.setLayout(new BorderLayout());
 
+        // the main panel has a boarder layout
+        // main panel has 2 panels- the center one for the letters with a grid layout - the south panel for the letters
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new GridLayout(3, 1));
-        mainPanel.setLayout(new GridLayout(3, 1));
 
         JTextField guessWord = new JTextField("Type your guess here: ");
         centerPanel.add(guessWord);
+        guessWord.setHorizontalAlignment(JTextField.CENTER);
 
+        //holds text/image but you cannot select them
         JLabel output = new JLabel();
         centerPanel.add(output);
+        output.setHorizontalAlignment(JTextField.CENTER);
 
-        mainPanel.add(centerPanel, BorderLayout.NORTH);
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
 
-        JButton button = new JButton("GUESS");
+        JButton button = new JButton("GUESS, Guess number: " + guessCounter);
         mainPanel.add(button, BorderLayout.SOUTH);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                CharResult[] result = currGame.guess(String.valueOf(guessWord));
-                output.setText(Arrays.toString(result));
-            }
+        button.addActionListener(e -> {
+            CharResult[] result = currGame.guess(guessWord.getText());
+            output.setText(Arrays.toString(result));
+            guessCounter++;
         });
 
         setContentPane(mainPanel);
